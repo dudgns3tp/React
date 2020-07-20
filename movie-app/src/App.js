f9bc4b1834afc918126f86006087b5236a9cca35
최영훈 <c_yh0327@naver.com>
2020-07-21 03:52:21 +0900

    React

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class App extends React.Component {
+  state = {
+    count : 0,
+  };
+
+  add = () =>{
+    this.setState(current =>({
+      count : current.count + 1
+    }))
+  }
+
+  minus = () =>{
+    this.setState(current =>({
+      count : current.count - 1
+    }))
+  }
+
+  render(){
+    return (
+      <div>
+        <h1> I'm a class Component {this.state.count} </h1>
+        <button onClick = {this.add}> Add </button>
+        <button onClick = {this.minus}> Minus</button>
+    </div>
+    );
+  }
+}
+
+/*
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name, picture, rating}){
+  console.log(foodLike.map(renderFood))
+  return (
+  <div>
+    <h1> I like {name}</h1>
+    <h4> {rating}/5.0</h4>
+    <img src = {picture} alt = {name} />
+  </div>
+  )
+}
+
+const renderFood = dish => <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating} />
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div> 
+    <h1> Hello </h1> 
+    {foodLike.map(renderFood)}
+  </div>
+  )
 }
 
+const foodLike  = [
+  {
+    id:1,
+    name:'kimchi',
+    image:"https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png",
+    rating : 5
+  },
+  {
+    id:2,
+    name:'pizza',
+    image:"https://post-phinf.pstatic.net/MjAxODAzMTNfMjEx/MDAxNTIwOTA4ODU0OTE5.WtZJXpkGz4HkkQ3cKfb5q3je4EDAzzL_mtNEICGYIbog.ddY72F-n8VJd3EusCa7CfW4Ds37PJoR23Qx1SXpF9xcg.JPEG/image_9931661511520908830333.jpg?type=w800_q75",
+    rating : 4.9
+  },
+  {
+    id:3,
+    name:'chicken',
+    image:"https://economychosun.com/query/upload/303/20190608214338_gubchoja.jpg",
+    rating : 3.9
+  },
+];
+
+Food.prototype = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number
+};
+*/
 export default App;
---
+++ movie-app/src/Potato.js
@@ -0,0 +1,6 @@
+import React from 'react';
+function Potato() {
+    return <h3> I love potat</h3>
+}
+
+export default Potato;
